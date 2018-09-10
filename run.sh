b1a99fe952e5a6e7f465ffaf555c7e396229aeb7
Theofilos Papapanagiotou <theofilos@ieee.org>
2018-09-10 23:33:39 +0200

    minor: add tests for all evaluation classes

--- run.sh
+++ run.sh
@@ -1,6 +1,10 @@
 javac -cp contest.jar Player12.java
 jar cmf MainClass.txt submission.jar Player12.class
-echo "===== Test dummy function ==========="
+echo "===== Test Sphere function ==========="
 java -jar testrun.jar -submission=Player12 -evaluation=SphereEvaluation -seed=1
-echo "===== Test one contest function ====="
+echo "===== Test Bent Cigar ====="
 java -jar testrun.jar -submission=Player12 -evaluation=BentCigarFunction -seed=1
+echo "===== Test Katsuura ====="
+java -jar testrun.jar -submission=Player12 -evaluation=KatsuuraEvaluation -seed=1
+echo "===== Test Schaffers ====="
+java -jar testrun.jar -submission=Player12 -evaluation=SchaffersEvaluation -seed=1
