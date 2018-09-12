3bf35a26e7745a4630be94491cebcb475106e11a
janaleible <jana.leible@web.de>
2018-09-12 11:42:46 +0200

    fix code style

--- Player12.java
+++ Player12.java
@@ -4,62 +4,59 @@ import org.vu.contest.ContestEvaluation;
 import java.util.Random;
 import java.util.Properties;
 
-public class Player12 implements ContestSubmission
-{
-	Random rnd_;
-	ContestEvaluation evaluation_;
-  private int evaluations_limit_;
+public class Player12 implements ContestSubmission {
 
-	public Player12()
-	{
-		rnd_ = new Random();
+	private Random random;
+	private ContestEvaluation contestEvaluation;
+	private int evaluationsLimit;
+
+	public Player12() {
+		this.random = new Random();
 	}
 
-	public void setSeed(long seed)
-	{
+	public void setSeed(long seed) {
 		// Set seed of algortihms random process
-		rnd_.setSeed(seed);
+		this.random.setSeed(seed);
 	}
 
-	public void setEvaluation(ContestEvaluation evaluation)
-	{
+	public void setEvaluation(ContestEvaluation evaluation) {
+
 		// Set evaluation problem used in the run
-		evaluation_ = evaluation;
+		this.contestEvaluation = evaluation;
 
 		// Get evaluation properties
-		Properties props = evaluation.getProperties();
+		Properties properties = evaluation.getProperties();
         // Get evaluation limit
-        evaluations_limit_ = Integer.parseInt(props.getProperty("Evaluations"));
+        this.evaluationsLimit = Integer.parseInt(properties.getProperty("Evaluations"));
 		// Property keys depend on specific evaluation
 		// E.g. doubl		e param = Double.parseDouble(props.getProperty("property_name"));
-        boolean isMultimodal = Boolean.parseBoolean(props.getProperty("Multimodal"));
-        boolean hasStructure = Boolean.parseBoolean(props.getProperty("Regular"));
-        boolean isSeparable = Boolean.parseBoolean(props.getProperty("Separable"));
+        boolean isMultimodal = Boolean.parseBoolean(properties.getProperty("Multimodal"));
+        boolean hasStructure = Boolean.parseBoolean(properties.getProperty("Regular"));
+        boolean isSeparable = Boolean.parseBoolean(properties.getProperty("Separable"));
 
 		// Do sth with property values, e.g. specify relevant settings of your algorithm
-        if(isMultimodal){
+        if(isMultimodal) {
             // Do sth
-        }else{
+        } else {
             // Do sth else
         }
     }
 
-	public void run()
-	{
+	public void run() {
+
 		// Run your algorithm here
 
-        int evals = 0;
+        int evaluations = 0;
         // init population
         // calculate fitness
-        while(evals<evaluations_limit_){
+        while(evaluations < this.evaluationsLimit){
             // Select parents
             // Apply crossover / mutation operators
-            double child[] = {0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0};
+            double[] child = {0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0};
             // Check fitness of unknown fuction
-            Double fitness = (double) evaluation_.evaluate(child);
-            evals++;
+            Double fitness = (double) this.contestEvaluation.evaluate(child);
+            evaluations++;
             // Select survivors
         }
-
 	}
 }
