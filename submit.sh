8531407e64e6097e11c2782e5c9f8a6dd978e881
Theofilos Papapanagiotou <theofilos@ieee.org>
2018-09-26 21:53:25 +0200

    debug submit script

--- submit.sh
+++ submit.sh
@@ -10,6 +10,8 @@ javac -cp contest.jar player12.java group12/*.java visual/*.java
 echo "packaging classes to submission.jar"
 jar cmf MainClass.txt submission.jar player12.class group12/*.class visual/*.class
 
+ls -l ./submit.jar
+
 curl -F 'teamcode=GG2o01g=' -F 'file=@./submission.jar' http://mac360.few.vu.nl:8080/EC_BB_ASSIGNMENT/FileUploader
 
 lynx -dump http://mac360.few.vu.nl:8080/EC_BB_ASSIGNMENT/rankings.html
