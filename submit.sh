aedc339417a5dd89c8a01a716da9a1b67309672c
Theofilos Papapanagiotou <theofilos@ieee.org>
2018-09-26 21:48:41 +0200

    debug submit script

--- submit.sh
+++ submit.sh
@@ -1,4 +1,14 @@
-./build.sh
+rm -f group12/*.class
+rm -f visual/*.class
+rm -f player12.class
+rm -f Player12.class
+rm -f submission.jar
+
+echo "compiling contest.jar"
+javac -cp contest.jar player12.java group12/*.java visual/*.java
+
+echo "packaging classes to submission.jar"
+jar cmf MainClass.txt submission.jar player12.class group12/*.class visual/*.class
 
 curl -F 'teamcode=GG2o01g=' -F 'file=@./submission.jar' http://mac360.few.vu.nl:8080/EC_BB_ASSIGNMENT/FileUploader
 
