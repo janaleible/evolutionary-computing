28187dd0d627ed5e72ba51eeb1763079ca516475
Joe Harrison <joeharrison1994@gmail.com>
2018-09-28 09:55:00 +0200

    Adding Rank based Selection

---
+++ group12/RankBasedSelection.java
@@ -0,0 +1,46 @@
+package group12;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Random;
+import java.util.Comparator;
+
+public class RankBasedSelection extends Selection{
+
+    private Random random;
+    private double sigma;
+
+    public RankBasedSelection(Random random, double sigma) {
+        this.random = random;
+        this.sigma = sigma;
+    }
+
+    @Override
+    public List<Individual> select(int numberOfPicks, List<Individual> population) {
+
+        population.sort(Comparator.comparingDouble(Individual::getFitness).reversed());
+
+        int mu = population.size();
+
+        double[] picks = new double[numberOfPicks];
+        for (int i = 0; i < numberOfPicks; i++) {
+            picks[i] = this.random.nextDouble();
+        }
+        Arrays.sort(picks);
+
+        List<Individual> parents = new ArrayList<>(numberOfPicks);
+        double runningTotal = 0;
+        int pickIndex = 0;
+        for (Individual individual : population) {
+            runningTotal += (2-this.sigma)/mu + 2*pickIndex*(this.sigma-1)/(mu*(mu-1));
+            while (picks[pickIndex] < runningTotal) {
+                parents.add(individual);
+                pickIndex++;
+                if (pickIndex >= numberOfPicks) { break; }
+            }
+            if (pickIndex >= numberOfPicks) { break; }
+        }
+        return parents;
+    }
+}
