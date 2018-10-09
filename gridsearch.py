748b8611aed4932de4f38cf6d9c977ad5c987051
janaleible <jana.leible@web.de>
2018-10-09 18:14:48 +0200

    script gridsearch

--- gridsearch.py
+++ gridsearch.py
@@ -1,30 +1,86 @@
 from collections import defaultdict
 import operator
 import os
+import sys
+import yaml
 
 functions = ['SphereEvaluation', 'BentCigarFunction', 'KatsuuraEvaluation', 'SchaffersEvaluation']
-
-for function in functions:
-
-    configuration_scores = defaultdict(lambda: 0)
-
-    for file_name in filter(lambda name: name.endswith(".txt"), os.listdir('gridsearch/{}/'.format(function))):
-
-            with open('gridsearch/{}/{}'.format(function, file_name)) as file:
-
-                properties = file.readline().strip('\n').split(', ')
-                for property in properties:
-                    (key, value) = property.split(': ')
-
-                    if key == 'Score':
-                        score = float(value)
-
-                    if key == 'configuration':
-                        id = value
+file_number = 0
 
 
-                configuration_scores[id] += score
+def write_config(config):
+    global file_number
+    with open('gridsearch/configurations/{}.yaml'.format(file_number), 'w') as file:
+        file.write(yaml.dump(config))
+    file_number += 1
 
-    topConfig = max(configuration_scores.items(), key=operator.itemgetter(1))[0]
 
-    print('Function {}: config {}, score {}'.format(function, topConfig, configuration_scores[topConfig] / 5)) # normalise by number of tested seeds
+def generate():
+    global file_number
+    for range_function in ['wrap', 'clip']:
+        for crossover in ['arithmetic', 'blend', 'random', 'uniform']:
+            for mutation in ['random', 'adaptive']:
+                for mutation_rate in [0.01, 0.3, 0.5]:
+                    for parent_selection in ['fitnessproportional', 'rankbased', 'tournament']:
+                        for elitism in ['true', 'false']:
+                            for population_size in [30, 100, 250]:
+                                for generation_gap in [0.5, 1]:
+                                    for crossover_rate in [0.8, 1]:
+
+                                        config = {
+                                            'rangefunction': range_function,
+                                            'crossover': crossover,
+                                            'mutation': mutation,
+                                            'parentselection': parent_selection,
+                                            'elitism': elitism,
+                                            'populationsize': population_size,
+                                            'generationgap': generation_gap,
+                                            'crossoverrate': crossover_rate,
+                                            'mutationrate': mutation_rate
+                                        }
+
+                                        if generation_gap < 1:
+                                            for survivor_selection in ['fitnessproportional', 'rankbased', 'tournament']:
+                                                config['survivorselection'] = survivor_selection
+
+                                                if parent_selection == 'tournament' or survivor_selection == 'tournament':
+                                                    for tournamentsize in [2, 5, 10]:
+                                                        config['tournamentsize'] = tournamentsize
+                                                        write_config(config)
+                                                else:
+                                                    write_config(config)
+                                        else:
+                                            write_config(config)
+
+    print(file_number)
+
+
+if __name__ == '__main__':
+
+    if sys.argv[1] == 'generate':
+        generate()
+
+# for function in functions:
+#
+#     configuration_scores = defaultdict(lambda: 0)
+#
+#     for file_name in filter(lambda name: name.endswith(".txt"), os.listdir('gridsearch/{}/'.format(function))):
+#
+#             with open('gridsearch/{}/{}'.format(function, file_name)) as file:
+#
+#                 properties = file.readline().strip('\n').split(', ')
+#                 for property in properties:
+#                     (key, value) = property.split(': ')
+#
+#                     if key == 'Score':
+#                         score = float(value)
+#
+#                     if key == 'configuration':
+#                         id = value
+#
+#
+#                 configuration_scores[id] += score
+#
+#     topConfig = max(configuration_scores.items(), key=operator.itemgetter(1))[0]
+#
+#     print('Function {}: config {}, score {}'.format(function, topConfig, configuration_scores[topConfig] / 5)) # normalise by number of tested seeds
--- gridsearch.sh
+++ gridsearch.sh
@@ -4,19 +4,32 @@ export LD_LIBRARY_PATH=.
 
 ./build.sh > /dev/null
 
-functions=("SphereEvaluation" "BentCigarFunction" "KatsuuraEvaluation" "SchaffersEvaluation")
-seeds=("01" "02" "03" "04" "05")
+mkdir -p gridsearch
+# mkdir -p gridsearch/configurations
 
-for function in ${functions[@]}; do
-    mkdir -p gridsearch/$function
+echo "generating configurations ..."
+# python3 gridsearch.py generate
 
-    echo "Testing on " $function
+function=$1
+number_of_seeds=$2
 
-    for seed in ${seeds[@]}; do
-        for config in {1..134}; do
-            echo "testing: " $config$seed
-            java -jar testrun.jar -submission=player12 -evaluation=$function -seed=$config$seed > gridsearch/$function/$config"_"$seed.txt
-        done
+#rm -r gridsearch/$function
+#mkdir -p gridsearch/$function
+
+echo "running ..."
+
+filename_regex="gridsearch/configurations/(.*).yaml"
+
+for filename in gridsearch/configurations/*.yaml; do
+
+    if [[ $filename =~ $filename_regex ]]
+    then
+        confignumber="${BASH_REMATCH[1]}"
+    fi
+
+    for ((seed=0; seed<$number_of_seeds; seed++)); do
+        echo $confignumber, $seed
+        params=$(python3 config2params.py $filename)
+        java $params -jar testrun.jar -submission=player12 -evaluation=$function -seed=$seed > gridsearch/$function/"$confignumber"_$seed.txt
     done
 done
-
