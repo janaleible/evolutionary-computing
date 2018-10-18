2b14f311c3667f54f347867af2fb1f2fa731b40a
janaleible <jana.leible@web.de>
2018-10-18 11:57:06 +0200

    setup rts experiment

--- run-all-experiments.sh
+++ run-all-experiments.sh
@@ -1,21 +1,21 @@
 #!/usr/bin/env bash
 
-./experiment.sh baseline BentCigarFunction 100 none
-./experiment.sh island BentCigarFunction 100 experiments/configs/BentCigarFunction-island.yaml
-./experiment.sh gender BentCigarFunction 100 experiments/configs/gender.yaml
-./experiment.sh rts BentCigarFunction 100 experiments/configs/BentCigar-rts.yaml
+# ./experiment.sh baseline BentCigarFunction 100 none
+# ./experiment.sh island BentCigarFunction 100 experiments/configs/BentCigarFunction-island.yaml
+# ./experiment.sh gender BentCigarFunction 100 experiments/configs/gender.yaml
+# ./experiment.sh rts BentCigarFunction 100 experiments/configs/BentCigar-rts.yaml
 
 ./experiment.sh rts SchaffersEvaluation 100 experiments/configs/Schaffers-rts.yaml
 ./experiment.sh rts KatsuuraEvaluation 100 experiments/configs/Katsuura-rts.yaml
 
-./experiment.sh taboo BentCigarFunction 100 experiments/configs/taboo.yaml
+# ./experiment.sh taboo BentCigarFunction 100 experiments/configs/taboo.yaml
 
-./experiment.sh gender SchaffersEvaluation 100 experiments/configs/gender.yaml
-./experiment.sh baseline SchaffersEvaluation 100 none
-./experiment.sh island SchaffersEvaluation 100 experiments/configs/SchaffersEvaluation-island.yaml
-./experiment.sh taboo SchaffersEvaluation 100 experiments/configs/taboo.yaml
+# ./experiment.sh gender SchaffersEvaluation 100 experiments/configs/gender.yaml
+# ./experiment.sh baseline SchaffersEvaluation 100 none
+# ./experiment.sh island SchaffersEvaluation 100 experiments/configs/SchaffersEvaluation-island.yaml
+# ./experiment.sh taboo SchaffersEvaluation 100 experiments/configs/taboo.yaml
 
-./experiment.sh taboo KatsuuraEvaluation 100 experiments/configs/taboo.yaml
-./experiment.sh gender KatsuuraEvaluation 100 experiments/configs/gender.yaml
-./experiment.sh island KatsuuraEvaluation 100 experiments/configs/KatsuuraEvaluation-island.yaml
-./experiment.sh baseline KatsuuraEvaluation 100 none
+# ./experiment.sh taboo KatsuuraEvaluation 100 experiments/configs/taboo.yaml
+# ./experiment.sh gender KatsuuraEvaluation 100 experiments/configs/gender.yaml
+# ./experiment.sh island KatsuuraEvaluation 100 experiments/configs/KatsuuraEvaluation-island.yaml
+# ./experiment.sh baseline KatsuuraEvaluation 100 none
